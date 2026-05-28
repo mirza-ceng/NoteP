cf9470546f91355f8ee7674ecb73cc2e8fe2ce79
Mirza German <2005mirza2005@gmail.com>
2026-05-28 18:59:01 +0300

    commit 1234567890abcdef1234567890abcdef12345678

--- src/main/java/com/example/demo/Controllers/PageController.java
+++ src/main/java/com/example/demo/Controllers/PageController.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/pages")
+@CrossOrigin(origins = "*")
 public class PageController {
 
     private final PageService pageService;
